6b8d198435574a0770664ca4712e743fc9bbfb1b
Andreu Orenga <andreuorenga@gmail.com>
2024-10-16 20:05:12 +0200

    Poject before up migrations

---
+++ database/migrations/2024_10_08_182145_create_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_crew_id')->constrained()->onDelete('cascade');
+            $table->decimal('import', 8, 2); // Cantidad con dos decimales
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
