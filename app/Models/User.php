9527e8147c66771c711c09bf1e76cfd2ac00a627
Andreu Orenga <andreuorenga@gmail.com>
2024-10-21 19:06:17 +0200

    User registration with email verification implemented successfully

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 }
+
