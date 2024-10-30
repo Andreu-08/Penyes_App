66c9b742d0a6a2ec3423863e29c0a85eea952f5f
Andreu Orenga <andreuorenga@gmail.com>
2024-10-30 19:04:38 +0100

    Clean welcome, blade rute /log

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/log', function () {
+    return view('log');
+})->middleware(['auth', 'verified'])->name('log');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
